5a89b5ebdc23a2cb80facc446daeed78e031502d
danparisd <43418833+danparisd@users.noreply.github.com>
2019-08-23 14:04:40 -0400

    Update btt_writer_resource_update.sh
    move to local node install

--- btt_writer_resource_update.sh
+++ btt_writer_resource_update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-npm list -g | grep door43-client || npm install -g door43-client
-door43-client index -u https://api.bibletranslationtools.org/v2/ts/catalog.json
-door43-client download
+npm install
+npm run index
+npm run download
 cd resource_containers
 for i in */; do (cd "$i" ; tar -cjvf "${i%/}.tsrc" * ; mv "${i%/}.tsrc" ../ ; cd .. ; rm -rf "$i"); done
 cd ..
