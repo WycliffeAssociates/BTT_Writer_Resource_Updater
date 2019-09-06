fae60877130437dfbf50d4fa320b37a4e84f3aa7
danparisd <43418833+danparisd@users.noreply.github.com>
2019-09-06 16:39:05 -0400

    add containers for BTT Writer android

--- btt_writer_resource_update.sh
+++ btt_writer_resource_update.sh
@@ -5,5 +5,6 @@ npm run index
 npm run download
 cd resource_containers
 for i in */; do (cd "$i" ; tar -cjvf "${i%/}.tsrc" * ; mv "${i%/}.tsrc" ../ ; cd .. ; rm -rf "$i"); done
+zip -r containers.zip *
 cd ..
 zip -r resource_containers.zip resource_containers/ index.sqlite
