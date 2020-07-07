dd2bfde95b7af76f674e84f93972633cc39a6f21
Dan <dan_paris@wycliffeassociates.org>
2020-07-07 13:44:08 -0400

    add english only zip

--- btt_writer_resource_update.sh
+++ btt_writer_resource_update.sh
@@ -6,6 +6,9 @@ npm run download
 cd resource_containers
 for i in */; do (cd "$i" ; tar -cjvf "${i%/}.tsrc" * ; mv "${i%/}.tsrc" ../ ; cd .. ; rm -rf "$i"); done
 zip -r containers.zip *
+zip -r containers-en.zip en*
 mv containers.zip ../
+mv containers-en.zip ../
 cd ..
 zip -r resource_containers.zip resource_containers/ index.sqlite
+
